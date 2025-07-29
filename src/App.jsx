50f990dd98dcb521c94580bf94ee34ff44103630
user.name "Aashutosh Dahal <user.email "aashutoshdahal91@gmail.com>
2025-07-29 21:21:16 +0545

    filter page merged

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GharBazaarAuth from "./pages/Login_Signup";
-
+import FilterPage from "./pages/FilterPage";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login-signup" element={<GharBazaarAuth />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/filter" element={<FilterPage />} />
       </Routes>
     </BrowserRouter>
   );
